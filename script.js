44a2e744111e37bcc496db26ffdc217fcc5f916e
komaldeswal1018-ai <komaldeswal1018@gmail.com>
2026-05-06 17:51:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const elements = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    if(position < window.innerHeight - 100){
+      el.classList.add('show');
+    }
+  });
+});
